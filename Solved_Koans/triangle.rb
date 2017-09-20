d582a7d6aa454fab94c822fa901ec2ed46bffeea
Beatriz MS <beatriz.montalban.s@gmail.com>
2017-09-20 13:32:49 +0200

    New solution of triangle.rb
    Refactoring of the solution of the project triangle.rb

--- Solved_Koans/triangle.rb
+++ Solved_Koans/triangle.rb
@@ -13,29 +13,48 @@
 # and
 #   about_triangle_project_2.rb
 #
-class TriangleError < StandardError
-end
-
 def triangle(a, b, c)
-  # WRITE THIS CODE
-  if (a <= 0) || (b <= 0) || (c <= 0)
+
+  if is_TriangleError?(a, b, c)
     raise TriangleError
   end
-  if  a == b && b == c
+
+  if is_equilateral?(a, b, c)
     return :equilateral
-  end
-  if (a == b && a + b > c || b == c && b + c > a|| a == c && a + c > b)
+  elsif is_isosceles?(a, b, c)
     return :isosceles
-  end
-  if (a == b && a + b <= c || b == c && b + c <= a|| a == c && a + c <= b)
-    raise TriangleError
-  end
-  if !(a == b && b == c)
+  elsif is_scalene?(a, b, c)
     return :scalene
   end
 
 end
 
+def is_equilateral?(a, b, c)
+  a == b && b == c
+end
+
+def is_isosceles?(a, b, c)
+  (a == b || b == c || a == c)
+end
+
+def is_scalene?(a, b, c)
+  !(a == b && b == c)
+end
+
+def is_TriangleError?(a, b, c)
+  is_zero_or_negative?(a, b, c) || is_an_invalid_isosceles?(a, b, c)
+end
+
+def any_side_zero_or_negative?(a, b, c)
+  a <= 0 || b <= 0 || c <= 0
+end
+
+def is_an_invalid_isosceles?(a, b, c)
+  (a == b && ((a + b) <= c)) ||
+  (b == c && ((b + c) <= a)) ||
+  (a == c && ((a + c) <= b))
+end
+
 # Error class used in part 2.  No need to change this code.
-#class TriangleError < StandardError
-#end
+class TriangleError < StandardError
+end
